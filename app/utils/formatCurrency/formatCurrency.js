ab32961a69a49becfa05bcf9e7b8c380cf555487
rburke <rburke@hotels.com>
2018-06-18 15:33:40 +0100

    fix(formatCurrency.js): Replaced toFixed call with toLocaleString's minimumFractionDigits parameter.
    fix #11
    
    Fix support for commas

--- app/utils/formatCurrency/formatCurrency.js
+++ app/utils/formatCurrency/formatCurrency.js
@@ -1,4 +1,4 @@
 module.exports = (amount, currency) => {
-  const decimalAmount = Number((amount / 100).toFixed(2));
-  return decimalAmount.toLocaleString(currency);
+  const decimalAmount = Number(amount / 100);
+  return decimalAmount.toLocaleString(currency, { minimumFractionDigits: 2 });
 };
