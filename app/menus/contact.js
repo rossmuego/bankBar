1a910f06f4f256dbd133341b19e409beb1168072
John <easton3003@gmail.com>
2018-07-18 17:56:45 +0100

    feat(contact): add slack link

--- app/menus/contact.js
+++ app/menus/contact.js
@@ -3,6 +3,7 @@ const { shell } = require('electron'); // eslint-disable-line
 module.exports = {
   label: 'Contact',
   submenu: [
+    { label: 'Slack', click() { shell.openExternal('https://join.slack.com/t/bankbar/shared_invite/enQtNDAwMjU0NDgwMzU2LWQ3NGI2M2UxZDhjOGQyODVhOGYzYTUzYzc1ZjczNjA3M2ZlMDc3ZTdhNjIyYmYwNzMwZDJmMTI2ODZhM2NjZjc'); } },
     { label: 'Twitter', click() { shell.openExternal('https://twitter.com/johneas10'); } },
     { label: 'Github', click() { shell.openExternal('https://github.com/johneas10'); } },
   ],
